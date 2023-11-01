537ca44dffc377c150473f115f724de9a1a9ee72
Johanna <johanna.werner@hof-university.de>
2023-11-01 23:46:14 +0100

    feat: added React Calendar

---
+++ src/components/Calendar.js
@@ -0,0 +1,58 @@
+import React, { useMemo, useState, useContext, useCallback, useEffect, useRef } from 'react';
+import { Calendar, Views, DateLocalizer, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'moment/locale/de';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import style from './Calendar.module.css';
+import AuthContext from '../AuthProvider';
+
+export default function CalendarComponent(props) {
+
+    const { auth, user } = useContext(AuthContext);
+
+    //define the formats
+    moment.locale('de');
+    const localizer = momentLocalizer(moment);
+    localizer.formats.dayRangeHeaderFormat = (({ start, end }, culture, localizer) =>
+        localizer.format(start, 'DD.', culture) + ' - ' + localizer.format(end, 'DD. MMMM', culture));
+    localizer.formats.dayFormat = 'dd DD.MM.';
+    localizer.formats.dayHeaderFormat = 'dddd, DD.MM.';
+    localizer.formats.agendaHeaderFormat = (({ start, end }, culture, localizer) =>
+        localizer.format(start, 'dddd, DD.MM.', culture) + ' - ' + localizer.format(end, 'dddd, DD.MM.', culture));
+    localizer.formats.agendaDateFormat = 'DD.MM.';
+
+   
+    //sets default values for the calendar
+    const { components, max, views } = useMemo(
+        () => ({
+            views: [Views.WEEK, Views.DAY, Views.AGENDA],
+        }),
+        []
+    )
+
+
+    return (
+        <div className={style.body} >
+            <Calendar
+                defaultView="week"
+                components={components}
+                /*events={appointments}*/
+                /*backgroundEvents={timeslots}*/
+                /*eventPropGetter={eventPropGetter}*/
+                localizer={localizer}
+                max={max}
+                showMultiDayTimes
+                step={30}
+                views={views}
+                /*onSelectEvent={handleClick}*/
+                dayLayoutAlgorithm="no-overlap"
+                tooltipAccessor={{}}
+                /*onSelectSlot={onSelectSlot}*/
+                selectable
+                culture='de'
+                style={style}
+                length={6}
+            />
+        </div>
+    )
+}
