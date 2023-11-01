7da271d0bd093f725e4dd1f4254a5f6cc713ac89
Johanna <johanna.werner@hof-university.de>
2023-11-01 23:47:52 +0100

    feat: added AssemblerDashboard

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import ManagerDashboard from './pages/ManagerDashboard';
+import AssemblerDashboard from './pages/AssemblerDashboard';
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login />} />
-        <Route path='/dashboard' element={<ManagerDashboard />} />
+        <Route path='/manager-dashboard' element={<ManagerDashboard />} />
+        <Route path='/assembler-dashboard' element={<AssemblerDashboard />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App;
