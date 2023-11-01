a4fdd6c3a0364d0ea0e0734525eecb7a90d0197a
Johanna <johanna.werner@hof-university.de>
2023-11-01 23:47:31 +0100

    fix: rearanged Button component

--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import style from './Button.module.css';
-
-function Button(props) {
-    return (
-        <input className={style[props.className]} type={props.type} value={props.label} />
-    )
-}
-
-export default Button;
