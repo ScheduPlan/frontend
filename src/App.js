c552fbd8926d9fbccf3bdee321995bb811edc9f1
Johanna <johanna.werner@hof-university.de>
2023-11-04 12:30:36 +0100

    feat: added pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { useContext, useNavigate } from 'react';
+import { AuthProvider } from './AuthProvider';
+
+// import pages
 import Login from './pages/Login';
 import ManagerDashboard from './pages/ManagerDashboard';
 import AssemblerDashboard from './pages/AssemblerDashboard';
+import AdminDashboard from './pages/AdminDashboard';
+import ChangePassword from './pages/ChangePassword';
+import CreateAppointment from './pages/CreateAppointment';
 
 export default function App() {
+  const { auth, user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setContent();
+  }, []); //auth, user
+
+  function setContent() {
+    if (user.is_admin === true) {
+      navigate("/dashboard");
+    } else if (user.is_manager === true) {
+      navigate("/dashboard");
+    } else if (user.is_assembler === true) {
+      navigate("/dashboard");
+    }
+  }
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/manager-dashboard' element={<ManagerDashboard />} />
-        <Route path='/assembler-dashboard' element={<AssemblerDashboard />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/admin' element={<AdminDashboard />} />
+          <Route path='/manager' element={<ManagerDashboard />} />
+          <Route path='/assembler' element={<AssemblerDashboard />} />
+          <Route path='/password' element={<ChangePassword />} />
+          <Route path='/appointment' element={<CreateAppointment />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
