5f5fcc870768a33a26db043afe97327b4d5ab0fd
Johanna <johanna.werner@hof-university.de>
2023-11-24 01:35:26 +0100

    feat: added functionality to create a team

--- src/forms/FormCreateTeam.js
+++ src/forms/FormCreateTeam.js
@@ -1,12 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Swal from 'sweetalert2';
 import axios from 'axios';
 import url from '../BackendURL';
+import { useNavigate } from 'react-router-dom';
+import { testEmployees } from '../UserExample';
+import Path from '../icons/Paths';
 
 export default function FormCreateTeam() {
 
+    const navigate = useNavigate();
+
     const [teamName, setTeamName] = useState("");
     const [teamDesc, setTeamDesc] = useState("");
+
+    const [allEmployees, setAllEmployees] = useState(testEmployees);
+    const [availableEmployees, setAvailableEmployees] = useState([]);
+    const [pickedEmployees, setPickedEmployees] = useState([]);
+
+    useEffect(() => {
+        //add all employees but in a different form
+        setAvailableEmployees(testEmployees.map(emp => {
+            return (
+                {
+                    id: emp.id,
+                    name: emp.firstName + " " + emp.lastName
+                }
+            )
+        }));
+    }, []);
 
     const getTeamName = (e) => {
         setTeamName(e.target.value);
@@ -16,12 +37,42 @@ export default function FormCreateTeam() {
         setTeamDesc(e.target.value);
     }
 
-    async function submitForm() {
+    const getPickedEmployees = (e) => {
+        const selectedOption = {
+            id: e.target.selectedOptions[0].id,
+            name: e.target.value
+        };
+        //add the picked element to pickedEmployees
+        setPickedEmployees((prevEmployees) => [...prevEmployees, selectedOption]);
+    }
+
+    function addEmployee() {
+        //remove the picked element from the avaiable ones
+        const filteredObjects = availableEmployees.filter(obj =>
+            !pickedEmployees.some(pickedObj => pickedObj.id === obj.id)
+        );
+        setAvailableEmployees(filteredObjects);
+    }
+
+    function removeEmployee(e) {
+        const selectedOption = pickedEmployees.find(elem => elem.id === e.target.id);
+        //console.log(selectedOption);
+
+        //remove the employee from the picked list
+        setPickedEmployees(pickedEmployees.filter(obj => obj != selectedOption));
+
+        //add the removed employee back to the select options
+        setAvailableEmployees((prevEmployees) => [...prevEmployees, selectedOption]);        
+    }
+
+    async function submitForm(event) {
+        event.preventDefault();
         try {
             const response = await axios.post(url + '/teams',
                 {
                     name: teamName,
                     description: teamDesc,
+                    employees: pickedEmployees
                 },
                 { headers: { 'Content-Type': 'application/json' } });
 
@@ -30,26 +81,18 @@ export default function FormCreateTeam() {
             Swal.fire({
                 position: 'top-end',
                 icon: 'success',
-                title: 'Neuer Auftrag angelegt!',
+                title: 'Neues Team angelegt!',
                 showConfirmButton: false,
-                timer: 5000
+                timer: 2000
             });
 
             setTimeout(function () {
-                window.location.reload();
+                navigate("..", { relative: "path" });
             }, 2500);
 
         } catch (error) {
             console.log(error);
         }
-    }
-
-    function testFct() {
-        axios.get(url + '/teams')
-            .then(res => {
-                const data = res.data;
-                console.log("data", data);
-            });
     }
 
     return (
@@ -58,7 +101,7 @@ export default function FormCreateTeam() {
             <form onSubmit={submitForm}>
                 <div className='form-row'>
                     <label>
-                        Teamname
+                        Name des Teams
                         <input className='light-blue' type="text" name="team" onChange={getTeamName} required />
                     </label>
                     <label>
@@ -66,8 +109,31 @@ export default function FormCreateTeam() {
                         <input className='light-blue' type="text" name="description" onChange={getTeamDesc} />
                     </label>
                 </div>
+                <div className='form-row'>
+                    <label>
+                        Mitarbeiter
+                        <select className='light-blue' name="customer" onChange={getPickedEmployees} required>
+                            <option readOnly hidden>Bitte wählen</option>
+                            {availableEmployees.map((emp) => {
+                                return (<option onClick={addEmployee} key={emp.id} id={emp.id} value={emp.name}>{emp.name}</option>)
+                            })}
+                        </select>
+                    </label>
+                    <div className='pickedItem-wrapper'>
+                        {(pickedEmployees != [] ?
+                            pickedEmployees.map((emp, index) => {
+                                return (
+                                    <div key={index} className='pickedItem'>
+                                        <p>{emp.name} {emp.lastName}</p>
+                                        <svg onClick={removeEmployee} id={emp.id} xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 -960 960 960">
+                                            <path d={Path("close")} />
+                                        </svg>
+                                    </div>
+                                )
+                            }) : "")}
+                    </div>
+                </div>
                 <input className="btn primary" type="submit" value="Anlegen" />
-                <input onClick={testFct} className="btn primary" type="button" value="AnlegenTest" />
             </form>
         </div>
     )
