518853f3868a8b47f57240c5d97a48979a6f45a5
Johanna <johanna.werner@hof-university.de>
2023-12-15 00:19:27 +0100

    feat: added fancy background

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -13,18 +13,20 @@ export default function Dashboard() {
         let content;
         if (window.location.href === "http://localhost:3000/" + TestUser.role) {
             content = (
-                <div className='content-container'>
-                    <h1>{TestUser.role} Dashboard</h1>
-                    <div className='button-wrapper'>
-                        {roles.find((r) => r.role === TestUser.role).links?.map((link, index) => (
-                            <Link key={index} to={link.path} className="btn primary" replace>{link.title}</Link> //hier machts immer zwei Mal die Rolle
-                        ))
-                        }
-                        <Link to='/password' className="btn primary" replace>Passwort ändern</Link>
-                        <Link to='/' className="btn primary" replace>Logout</Link>
-                    </div>
+                <div className='fancy-background'>
+                    <div className='content-container'>
+                        <h1>{TestUser.role} Dashboard</h1>
+                        <div className='button-wrapper'>
+                            {roles.find((r) => r.role === TestUser.role).links?.map((link, index) => (
+                                <Link key={index} to={link.path} className="btn primary" replace>{link.title}</Link> //hier machts immer zwei Mal die Rolle
+                            ))
+                            }
+                            <Link to='/password' className="btn primary" replace>Passwort ändern</Link>
+                            <Link to='/' className="btn primary" replace>Logout</Link>
+                        </div>
 
-                    <Outlet />
+                        <Outlet />
+                    </div>
                 </div>
             )
         }
