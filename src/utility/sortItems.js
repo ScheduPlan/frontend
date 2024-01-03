d4e0ad98002d8355aca1e92c507e10ca371bb20e
Johanna <johanna.werner@hof-university.de>
2024-01-03 17:47:46 +0100

    fix: added 2nd sort option to sort util function

--- src/utility/sortItems.js
+++ src/utility/sortItems.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react'
 
-export default function sortItems(objects, itemFilter) {
+export default function sortItems(objects, sort1, sort2) {
 
   return (
-    objects.sort(function (a, b) {
-        if (a[itemFilter] < b[itemFilter]) { return -1; }
-        if (a[itemFilter] > b[itemFilter]) { return 1; }
+    sort2 != null ?
+      objects.sort(function (a, b) {
+        if (a[sort1][sort2] < b[sort1][sort2]) { return -1; }
+        if (a[sort1][sort2] > b[sort1][sort2]) { return 1; }
+        return 0;
+      }) :
+      objects.sort(function (a, b) {
+        if (a[sort1] < b[sort1]) { return -1; }
+        if (a[sort1] > b[sort1]) { return 1; }
         return 0;
       })
   )
