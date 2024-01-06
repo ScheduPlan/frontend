1b873f35ab10eb3b4da9d6f42f0624a3125b81db
Johanna <johanna.werner@hof-university.de>
2024-01-06 17:00:38 +0100

    fix: refactored function to show events for each team

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -8,20 +8,41 @@ import sortItems from '../utility/sortItems'
 
 export default function Schedule() {
   const [teams, setTeams] = useState([]);
+  const [events, setEvents] = useState([]);
   const [orders, setOrders] = useState([]);
 
   const [activeOrder, setActiveOrder] = useState({});
 
   const [activeTeamId, setActiveTeamId] = useState("");
+  const [activeEvents, setActiveEvents] = useState([]);
 
   useEffect(() => {
     getTeams();
+    getEvents();
     getOrders();
   }, []);
 
+  useEffect(() => {
+    getActiveEvents();
+  }, [activeTeamId]);
+
+  /**
+   * gets all teams from API, sorts them & sets the setActiveTeamId as the first element
+   */
   function getTeams() {
     axios.get(url + "/teams").then(res => {
-      setTeams(sortItems(res.data, "description", "name"));
+      const t = sortItems(res.data, "description", "name");
+      setTeams(t);
+      setActiveTeamId(t.at(0).id);
+    });
+  }
+
+  /**
+   * gets all events from API
+   */
+  function getEvents() {
+    axios.get(url + "/events").then(res => {
+      setEvents(res.data);
     });
   }
 
@@ -32,24 +53,10 @@ export default function Schedule() {
   }
 
   /**
-   * opens team tab with specific calendar & sets the current team id
-   * @param {*} e 
+   * filters all events for currently active team
    */
-  function openTab(e) {
-    var i;
-    var x = document.getElementsByClassName(style.tab_container);
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    document.getElementById("team " + e.target.id).style.display = "block";
-
-    var y = document.getElementsByClassName("btn tab");
-    for (i = 0; i < y.length; i++) {
-      y[i].classList.remove("active");
-    }
-    document.getElementById(e.target.id).classList.add("active");
-
-    setActiveTeamId(e.target.id);
+  function getActiveEvents() {
+    setActiveEvents(events.filter(event => (event.order?.team?.id == activeTeamId)));
   }
 
   return (
@@ -58,13 +65,13 @@ export default function Schedule() {
         <h1>Planungsassistent</h1>
       </div>
       <div className='content-wrapper'>
-        <Sidebar activeTeamId={activeTeamId} activeOrder={(e) => setActiveOrder(e)}/>
+        <Sidebar activeTeamId={activeTeamId} activeOrder={(e) => setActiveOrder(e)} />
 
         <div className={style.calendar_wrapper}>
           <div className={style.tab_bar}>
             {teams.map(team => {
               return (
-                <button key={team.id} className="btn tab" id={team.id} onClick={openTab}>{team.description.name}</button>
+                <button key={team.id} className={"btn tab " + ((activeTeamId == team.id) ? "active" : "")} id={team.id} onClick={() => setActiveTeamId(team.id)}>{team.description.name}</button>
               )
             })}
           </div>
@@ -72,7 +79,7 @@ export default function Schedule() {
           {teams.map(team => {
             return (
               <div key={team.id} id={"team " + team.id} className={style.tab_container}>
-                <Calendar activeOrder={activeOrder} />
+                <Calendar events={activeEvents} activeOrder={activeOrder} />
               </div>)
           })}
         </div>
