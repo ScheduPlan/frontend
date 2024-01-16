16126f552157add502ea4fb4d500d9edf03659b1
Johanna <johanna.werner@hof-university.de>
2024-01-16 21:59:38 +0100

    feat: added function to sort in 3 depths

--- src/utility/sortItems.js
+++ src/utility/sortItems.js
@@ -1,18 +1,30 @@
-import React, { useEffect, useState } from 'react'
+export default function sortItems(objects, sort1, sort2, sort3) {
 
-export default function sortItems(objects, sort1, sort2) {
-
-  return (
-    sort2 != null ?
-      objects.sort(function (a, b) {
-        if (a[sort1][sort2] < b[sort1][sort2]) { return -1; }
-        if (a[sort1][sort2] > b[sort1][sort2]) { return 1; }
-        return 0;
-      }) :
+  if (sort2 != null ) {
+    if(sort3 != null) {
+      return (
+        objects.sort(function (a, b) {
+          if (a[sort1][sort2][sort3] < b[sort1][sort2][sort3]) { return -1; }
+          if (a[sort1][sort2][sort3] > b[sort1][sort2][sort3]) { return 1; }
+          return 0;
+        })
+      )
+    } else {
+      return (
+        objects.sort(function (a, b) {
+          if (a[sort1][sort2] < b[sort1][sort2]) { return -1; }
+          if (a[sort1][sort2] > b[sort1][sort2]) { return 1; }
+          return 0;
+        })
+      )
+    }
+  } else {
+    return (
       objects.sort(function (a, b) {
         if (a[sort1] < b[sort1]) { return -1; }
         if (a[sort1] > b[sort1]) { return 1; }
         return 0;
       })
-  )
+    )
+  }
 }
