fc60620100e4977f4edcae1ce8283371624e1135
Johanna <johanna.werner@hof-university.de>
2024-01-24 02:43:42 +0100

    fix: edited order component

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import popupStyle from './PopUp.module.css';
 
 export default function Order(props) {
+  const [order, setOrder] = useState({});
+
+  useEffect(() => {
+    setOrder(props.object);
+  }, [props.object]);
+
+  console.log(props.object);
   return (
     props.extended ?
       <>
-        <h2>{props.object.commissionNumber} {props.object.customer?.company}</h2>
+        <h2>Auftragsnummer {order.number}</h2>
         <div className={popupStyle.popup_details}>
-          <p><b>Ansprechpartner: </b>{props.object.customer?.firstName} {props.object.customer?.lastName}</p>
-          <p><b>Gewicht: </b>{props.object.weight}kg</p>
+          <p><b>Kommisionsnummer: </b>{order.commissionNumber}</p>
+          <p><b>Kunde: </b>{order.customer?.customerNumber} {order.customer?.company}</p>
+          <p><b>Ansprechpartner: </b>{order.customer?.firstName} {order.customer?.lastName}</p>
+          <p><b>Gewicht: </b>{order.weight}kg</p>
+          <p><b>Geschätzte Dauer: </b>{order.plannedDuration}h</p>
+          <p><b>Team: </b>{order.team?.description?.name}</p>
         </div>
       </> :
       <div>
-        {props.object.commissionNumber} {props.object.customer?.company}
+        <b>{order.number}</b> {order.customer?.company} ({order.customer?.customerNumber})
       </div>
   )
 }
